f520bb0c88d872e72b1c4cdb3164d62644ce70ca
kuljeetsingh <kuljeetsingh951@gmail.com>
2019-07-22 00:12:16 +0530

    commit

--- week-3 programming assignment/car_fuel/car_fuel.cpp
+++ week-3 programming assignment/car_fuel/car_fuel.cpp
@@ -1,29 +1,66 @@
-#include <algorithm>
-#include <iostream>
-#include <stdexcept>
-#include <vector>
+#include<iostream>
+#include<vector>
 
-int count_stops(int D, const std::vector<int>& d) {
-  if (d.size() < 2) { throw std::logic_error("Vector too small."); }
+/*
+    ********************SAFE MOVE**********************
+    Moving as far as the fuel is good enought to reach another gas station
+*/
 
-  auto p = std::begin(d);
-  auto count = 0;
+using namespace std;
 
-  while (p != --std::end(d)) {
-    // Greedy: go as far as I can on this tank of gas.
-    auto n = --std::find_if(p, std::end(d), [=](int x) {
-      return *p + D < x; });
-    // The specification says we do not need to worry about this...
-    if (p == n) { throw std::logic_error("D too small."); }
-    p = n;
-    ++count; }
+    /*
+        * This program has Loop nested within another loop
+        * So, it seems to have O(n * n) run-time
+        * But, the run time of the program is O(n)
+            => currentRefills can be atmost n - 1
+            => numRefills can be atmost n
+            => So there will be atmost n + 1 operations
+                => O(n + 1) => O(n)
+    */
 
-  return count; }
+int minRefills(int n, vector<int> x, int dist_with_full_tank)
+{
+    int numRefills = 0, currentRefills = 0;
+    int lastRefills = 0;
+    //cout<<"Stops where you need to get refilling:";
+    while(currentRefills < (n - 1))
+    {
+        lastRefills = currentRefills;
+        while((currentRefills < (n - 1)) && (x[currentRefills + 1] - x[lastRefills] <= dist_with_full_tank))
+        {
+            currentRefills++;
+            if(currentRefills == (n - 1))
+                break;
+        }
+        //cout<<a[lastRefills]<<'\n';
+        if(currentRefills == lastRefills)
+        // This happens when gas station is far away that even full tank of fuel 
+        // could not make up
+            return -1;
+        if(currentRefills < (n - 1))
+            numRefills++;
+    }
+    return numRefills;
+}
 
+main()
+{
+    int n = 0;
+    vector<int> x;
+    int dist_with_full_tank = 0;
 
-int main(int, char* []) {
-  auto D = 16;
-  auto d = std::vector<int> { 0, 5, 15, 30, 32, 33, 37, 49, 53, 59, 61 };
+    //cout<<"Give n:"
+    cin>>n;
+    int temp = 0;
+    //cout<<"Give gas station stops in kms:";
+    for(int i = 0; i < n; i++)
+    {
+        cin>>temp;
+        x.push_back(temp);
+    }
+    //cour<<"Give Mileage per full tank:";    
+    cin>>dist_with_full_tank;
 
-  std::cout << "stops: " << count_stops(D, d) << "\n";
-  return 0; }
+    //cout<<"Number of refilling required:";
+    cout<<minRefills(n, x, dist_with_full_tank)<<'\n';
+}
